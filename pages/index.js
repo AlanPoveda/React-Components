6a0b5afd8091d5d91c4515b5eb16f0764a0108b5
Alan Poveda <alsepoveda@gmail.com>
2021-11-12 23:02:05 -0300

    Change scss to css, test the deploy. Change import

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 
-import Header from "./src/components/Header/index"
-import RightMenu from './src/components/RightMenu/index'
-import NavBar from './src/components/NavBar/index'
-import ContentWidget from './src/components/contentWidget/index'
+import Header from "./src/components/Header/index.jsx"
+import RightMenu from './src/components/RightMenu/index.jsx'
+import NavBar from './src/components/NavBar/index.jsx'
+import ContentWidget from './src/components/contentWidget/index.jsx'
 
 export default function Home() {
   return (
