21fd5cb4e7e8460f245abe0deae12bcd850ecbab
Alan Poveda <alsepoveda@gmail.com>
2021-11-17 19:58:45 -0300

    Header fixed build

--- pages/src/components/Header/index.jsx
+++ pages/src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import styles from "./styles.module.scss"
 
 
-export function Header(){
+export default function Header(){
     return(
         <div>
             <h1 className={styles.title}>Header</h1>
