acaa22b40090bbe6c0ca4f372ea5a3538e09004b
Alan Poveda <alsepoveda@gmail.com>
2021-11-17 19:53:02 -0300

    Header

---
+++ pages/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss"
+
+
+export function Header(){
+    return(
+        <div>
+            <h1 className={styles.title}>Header</h1>
+        </div>
+    );
+}
