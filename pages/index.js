a2babfb0c1fbb82fa841bf989e65502da1e6ad0b
Alan Poveda <alsepoveda@gmail.com>
2021-11-17 19:20:21 -0300

    Init again the project

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,12 @@
-import Image from 'next/image'
 
 
-//import Header from "./src/components/Header/index"
-//import RightMenu from './src/components/RightMenu/index'
-//import NavBar from './src/components/NavBar/index'
-//import ContentWidget from './src/components/ContentWidget/index'
+
+
 
 export default function Home() {
   return (
     <div>
       
-      <div>
-      
-      </div>
     </div>
   )
 }
