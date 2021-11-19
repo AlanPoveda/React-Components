a1e578efb399ca3370ea6807946cfc278e504bfd
Alan Poveda <alsepoveda@gmail.com>
2021-11-18 22:57:53 -0300

    Content component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import  Header  from "./src/components/Header"
+import Content from "./src/components/Content"
 
 export default function Home() {
   return (
     <div>
       <Header />
-      
+      <Content />
     </div>
   )
 }
---
+++ pages/src/components/Content/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss"
+
+export default function Content(){
+
+
+    return(
+        <div>
+            <p className={styles.text}>This is a content</p>
+        </div>
+    );
+}
