317fe8ee1319ae1919cc178802e611d133bbc499
Alan Poveda <alsepoveda@gmail.com>
2022-02-08 22:21:51 -0300

    Install prismic client, and make the connection, to see the posts

--- src/pages/knowledge.tsx
+++ src/pages/knowledge.tsx
@@ -1,6 +1,11 @@
 import Header from "../components/Header";
 import { SideBar } from "../components/SideBar";
 import ContentKnowledge from "../components/ContentKnowledge";
+import { GetStaticProps } from 'next'
+
+
+import { getPrismicClient } from '../services/prismicConfiguration';
+import Prismic from '@prismicio/client' 
 
 
 
@@ -19,3 +24,21 @@ export default function KnowlagePage(){
 
 }
 
+//Chamada da api apra carregar a página uma unica vez, ainda mais para não consumir a banda
+export const getStaticProps: GetStaticProps = async () => {
+
+    const prismic = await getPrismicClient()
+
+    const response = await prismic.query([
+        Prismic.predicates.at('document.type', 'widget-information' )
+    ], {
+        fetch: ['title', 'content'],
+        pageSize: 100
+    })
+
+    console.log(JSON.stringify(response, null, 2))
+
+    return {
+        props: {}
+    }
+}
